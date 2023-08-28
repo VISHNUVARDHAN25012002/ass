556dfe7d43955329c5dd53ed86b94b5de08b55b8
VISHNUVARDHAN25012002 <vishnuvardhanrv25@gmail.com>
2023-08-28 23:36:02 +0530

    ass

---
+++ Q1/ProjectDeSeriailizer.java
@@ -0,0 +1,32 @@
+package Q1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class ProjectDeSeriailizer{
+	Map<Project,ArrayList<Employee>> employee;
+
+    @SuppressWarnings("unchecked")
+	public Map<Project,ArrayList<Employee>> projectDeSeriailizer() {
+	try {
+		FileInputStream fileInput=new FileInputStream("C:\\Users\\maste\\eclipse-workspace\\Assesment\\src\\Q1\\file.txt");
+		ObjectInputStream in=new ObjectInputStream(fileInput);
+		employee=(Map<Project, ArrayList<Employee>>) in.readObject();
+	}catch(FileNotFoundException e) {
+		e.printStackTrace();
+	}catch(ClassNotFoundException cnfe) {
+		cnfe.printStackTrace();
+	}catch(IOException ioe) {
+		ioe.printStackTrace();
+	}
+	
+	return employee;
+
+}
+
+}
---
+++ Q3/Q3.java
@@ -0,0 +1,16 @@
+package Q3;
+
+import java.util.Arrays;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int oddSquaresSum = Arrays.stream(numbers)
+            .filter(number -> number % 2 != 0) // Filter odd numbers
+            .map(oddNumber -> oddNumber * oddNumber) // Square the odd numbers
+            .sum(); // Calculate the sum of squared odd numbers
+
+        System.out.println(oddSquaresSum); // Output: 35
+    }
+}
